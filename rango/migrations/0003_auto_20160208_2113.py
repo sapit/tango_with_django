75693d48fdafa5a02ab02797f1103c73f41f101a
sapit <zdravkozzz13@gmail.com>
2016-02-08 22:10:07 +0000

    Finished chapter 7 with additional ex

---
+++ rango/migrations/0003_auto_20160208_2113.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rango', '0002_auto_20160208_2015'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='category',
+            name='slug',
+            field=models.SlugField(default=''),
+            preserve_default=False,
+        ),
+        migrations.AlterField(
+            model_name='page',
+            name='category',
+            field=models.ForeignKey(related_name=b'pages', to='rango.Category'),
+        ),
+    ]
