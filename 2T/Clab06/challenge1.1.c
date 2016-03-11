9308684abc39b63e2faa75bb7d77927ac5a55dc2
sapit <zdravkozzz13@gmail.com>
2016-03-11 16:05:17 +0000

    finished Clab06 but last challenge question

--- 2T/Clab06/challenge1.1.c
+++ 2T/Clab06/challenge1.1.c
@@ -7,33 +7,28 @@ int main(void)
 {
     FILE *in_ptr, *out_ptr;
     char fname[MAX], lname[MAX], number[12], buffer[1000];
-    in_ptr = fopen("phonelist.txt", "rb");
+    in_ptr = fopen("phonelist.txt", "r");
     out_ptr = fopen("history.txt", "wb");
     if((NULL == in_ptr) || (NULL == out_ptr))
     {
         fputs("Problem opening files, exiting! \n", stderr);
         exit(1);
-   } 
-   fread(number, sizeof(number), 1, in_ptr);
-   fread(buffer, sizeof(buffer), 2, in_ptr);
-//   fread(lname,sizeof(lname),1,in_ptr);
+    } 
 
-   printf("%s %s \n", number, buffer);
-   fwrite(number, sizeof(number), 1, out_ptr);
-   fwrite(buffer, sizeof(buffer), 2, out_ptr);
-//   fwrite(fname, sizeof(fname), 1, out_ptr);
-//   fwrite(lname, sizeof(lname), 1, out_ptr);
-    // if(0 <= fscanf(in_ptr, FORMATREAD, number, fname, lname)) //double == instead of just single = and in_ptr is needed
-    // {
-    //     fprintf(out_ptr, "%s %s %s\n", number, fname, lname);
-    //     printf("%s %s %s\n", number, fname, lname);
-    //     printf("the phonebook has format: phone number - name\n");
-    // }
-    // else
-    // {
-    //     fprintf(out_ptr, "Unknown type\n");    //we need the ptr not its value
-    //     printf("The phone book has unknown format \n");
-    // }
+    if(0 <= fscanf(in_ptr, FORMATREAD, number, fname, lname)) //double == instead of just single = and in_ptr is needed
+    {
+        fwrite(number, sizeof(number), 1, out_ptr);
+        fwrite(fname, sizeof(fname), 1, out_ptr);
+        fwrite(lname, sizeof(lname), 1, out_ptr);
+        
+        printf("%s %s %s\n", number, fname, lname);
+        printf("the phonebook has format: phone number - name\n");
+    }
+    else
+    {
+        fwrite(&("Unknown type\n"),sizeof("Unknown type\n"),1, out_ptr);    //we need the ptr not its value
+        printf("The phone book has unknown format \n");
+    }
     fclose(in_ptr);
     fclose(out_ptr);   //needs to be out_ptr instead of *out_ptr 
     return 0;
